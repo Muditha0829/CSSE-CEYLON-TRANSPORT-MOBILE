cf1676c1b0c677739a9fbd69f5884c68eadef834
Muditha0829 <muditha1gms@gmail.com>
2022-11-09 08:22:37 +0530

    Commit 0

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -79,7 +79,7 @@ class _HomeState extends State<Home> {
                },
             ),
              ListTile(
-              title:  Text('Payment Methods'),
+              title:  const Text('Payment Methods'),
                onTap: (){
                  // Navigator.pop(context);
                },
